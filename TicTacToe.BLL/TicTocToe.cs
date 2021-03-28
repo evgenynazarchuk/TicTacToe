a708ce095f5c961505bbfbd91c36b18c96257ece
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-03-28 15:38:26 +0300

    add bll

---
+++ TicTacToe.BLL/TicTocToe.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.BLL
+{
+    public class TicTocToe
+    {
+        public GameStatus GameStatus { get; private set; } = GameStatus.Play;
+        public char[,] GameField { get; set; } = new char[3, 3];
+
+        public int CurrentPlayer { get; private set; } = 1;
+        public char UserSymbol { get; private set; } = 'X';
+
+        public TicTocToe()
+        {
+        }
+
+        public bool FindMatches()
+        {
+            bool gameOver = false;
+
+            if (this.GameField[0, 0] == this.UserSymbol && this.GameField[0, 1] == this.UserSymbol && this.GameField[0, 2] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+            else if (this.GameField[1, 0] == this.UserSymbol && this.GameField[1, 1] == this.UserSymbol && this.GameField[1, 2] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+            else if (this.GameField[2, 0] == this.UserSymbol && this.GameField[2, 1] == this.UserSymbol && this.GameField[2, 2] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+
+            else if (this.GameField[0, 0] == this.UserSymbol && this.GameField[1, 0] == this.UserSymbol && this.GameField[2, 0] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+            else if (this.GameField[0, 1] == this.UserSymbol && this.GameField[1, 1] == this.UserSymbol && this.GameField[2, 1] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+            else if (this.GameField[0, 2] == this.UserSymbol && this.GameField[1, 2] == this.UserSymbol && this.GameField[2, 2] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+
+            else if (this.GameField[0, 0] == this.UserSymbol && this.GameField[1, 1] == this.UserSymbol && this.GameField[2, 2] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+            else if (this.GameField[0, 2] == this.UserSymbol && this.GameField[1, 1] == this.UserSymbol && this.GameField[2, 0] == this.UserSymbol)
+            {
+                gameOver = true;
+            }
+
+            if (gameOver)
+            {
+                this.GameStatus = GameStatus.GameOver;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsValidCellNumber(int number)
+        {
+            if (number >= 1 && number <= 9)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public char GetCellValueByPosition(CellPosition cellPosition)
+        {
+            return this.GameField[cellPosition.i, cellPosition.j];
+        }
+
+        public CellPosition FindCellPositionByNumber(int number)
+        {
+            int cellNumber = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (cellNumber == number)
+                    {
+                        return new CellPosition(i, j);
+                    }
+                    cellNumber++;
+                }
+            }
+            throw new ApplicationException("The number must be between 1 and 9.");
+        }
+
+        public bool CheckAccessCellByPosition(CellPosition cellPostion)
+        {
+            if (this.GetCellValueByPosition(cellPostion) == 0)
+                return true;
+            else return false;
+        }
+
+        public void SetCellSymbol(CellPosition cellPosition)
+        {
+            this.GameField[cellPosition.i, cellPosition.j] = this.UserSymbol;
+        }
+
+        public void ChangePlayer()
+        {
+            this.CurrentPlayer = this.CurrentPlayer == 1 ? 2 : 1;
+            this.UserSymbol = this.CurrentPlayer == 1 ? 'X' : 'O';
+        }
+
+        public void PrintGameField()
+        {
+            int cellNumber = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (this.GameField[i, j] == 0)
+                    {
+                        Console.Write(cellNumber);
+                    }
+                    else
+                    {
+                        Console.Write(this.GameField[i, j]);
+                    }
+                    Console.Write(" ");
+                    cellNumber++;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintWinner()
+        {
+            Console.Write($"Congratulations winner player {this.CurrentPlayer}");
+        }
+    }
+}
---
+++ TicTacToe.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TicTacToe.BLL;
+
+namespace TicTacToe.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new TicTocToe();
+            int userInputCellNumber;
+
+            while (game.GameStatus != GameStatus.GameOver)
+            {
+                Console.Clear();
+                game.PrintGameField();
+
+                Console.Write(game.CurrentPlayer == 1 ? "Gamer X, " : "Gamer O, ");
+                Console.Write("enter field number: ");
+                try
+                {
+                    userInputCellNumber = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                var cellPosition = game.FindCellPositionByNumber(userInputCellNumber);
+                if (game.CheckAccessCellByPosition(cellPosition))
+                {
+                    game.SetCellSymbol(cellPosition);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (game.FindMatches())
+                {
+                    Console.Clear();
+                    game.PrintGameField();
+                    Console.Write($"Congratulations winner player {game.CurrentPlayer}");
+                    break;
+                }
+                else
+                {
+                    game.ChangePlayer();
+                }
+            }
+        }
+    }
+}
